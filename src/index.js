0e1b7422c2c49a79dce899df53e4b0cd889cf28b
Peacemaker84 <mazdat76@gmail.com>
2022-12-14 22:23:58 +0300

    Add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from "react"; // node_modules/react/index.js
+import ReactDOM from "react-dom"; // node_modules/react-dom/index.js
+import "bootstrap/dist/css/bootstrap.css"; // node_modules/bootstrap/.../file.css
+import "./index.css"; // "эта папка/file.css"
 import App from "./App";
 
-ReactDOM.render(
-	<App/>,
-	document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
